9bbf36c7215e7e5dd04dbd1958084cafab2865aa
Gino Bollaert <gbollaert@gmail.com>
2022-10-05 16:17:37 +0200

    install target

--- cmake/AppInfo.cmake
+++ cmake/AppInfo.cmake
@@ -5,13 +5,16 @@
 ##  Created by Gino Bollaert on 04/10/2022.
 ##
 
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
 set(APP_NAME "Template")
 set(APP_VERSION 0.1.0)
 set(COMPANY Company)
 set(IDENTIFIER com.company.qt-template)
 set(COPYRIGHT "Copyright (c) 2022 Gino Bollaert. All rights reserved.")
 set(ICON_NAME melodica.ico)
-set(IS_AGENT FALSE)
+set(MACOSX_IS_AGENT FALSE)
 
 set(SOURCE_FILES
     src/main.cpp
---
+++ cmake/Qt.cmake
@@ -0,0 +1,12 @@
+##
+##  Qt.cmake
+##  qt6-cmake-vscode
+##
+##  Created by Gino Bollaert on 05/10/2022.
+##
+
+if (APPLE)
+    set(QT6_DIR ~/Qt/6.4.0/macos)
+elseif (WIN32)
+    set(QT6_DIR c:/Qt/6.4.0/mingw_64)
+endif()
